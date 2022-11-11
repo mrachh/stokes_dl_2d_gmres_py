7f844b7e4c186f28abb975e13c135067052122c7
Manas Rachh <mrachh@flatironinstitute.org>
2022-11-11 15:11:51 -0500

    fixing gmres error

--- stokes_sherman_lauricella_solver.f
+++ stokes_sherman_lauricella_solver.f
@@ -1,5 +1,5 @@
       subroutine lpcomp_stok_dl_interior(n,src,dsdt,rn,rkappa,dwgt,pwgt,
-     1   eps,dens,vel)
+     1   eps,rdens,rvel)
 c
 c   This subroutine applies the sherman lauricella version 
 c   representation of the stokes double layer potential with an added
@@ -34,12 +34,12 @@ c
       integer, intent(in) :: n
       real *8, intent(in) :: src(2,n),dsdt(n),rn(2,n),rkappa(n),dwgt
       real *8, intent(in) :: pwgt,eps
-      complex *16, intent(in) :: dens(n)
-      complex *16, intent(out) :: vel(n)
+      real *8, intent(in) :: rdens(2*n)
+      real *8, intent(out) :: rvel(2*n)
 
       complex *16 grad(2),hess(3),veltarg,gradtarg(2),hesstarg(3)
       complex *16 charge
-      complex *16, allocatable :: dip(:,:)
+      complex *16, allocatable :: dip(:,:),vel(:),dens(:)
       complex *16 zu,zn,z1,z2,ztmp,ima,zx,zw
       integer nt
       real *8 targ(2)
@@ -52,10 +52,11 @@ c
       ifcharge = 0
       ifdipole = 1
 
-      allocate(dip(2,n))
+      allocate(dip(2,n),dens(n),vel(n))
 
 C$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(i,zu,zn)      
-      do i=1,n
+      do i=1,n 
+        dens(i) = rdens(2*i-1) + ima*rdens(2*i)
         zu = -0.25d0*dens(i)*dsdt(i)/pi
         zn = rn(1,i) + ima*rn(2,i)
         dip(1,i) = -dwgt*zu*zn
@@ -90,9 +91,11 @@ C$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(zn,zw,zx,i)
         zn = rn(1,i) + ima*rn(2,i)
         zw = -0.25d0*dsdt(i)*rkappa(i)/pi
         zx = -0.25d0*dsdt(i)*rkappa(i)*zn*zn/pi
-        vel(i) = vel(i) + pwgt*dimag(velpert*zn)
+        vel(i) = vel(i) + pwgt*velpert*zn
         vel(i) = vel(i) + dwgt*( (zw+zx)*dreal(dens(i)) +
-     1       ima*(zw-zx)*dimag(dens(i))) 
+     1       ima*(zw-zx)*dimag(dens(i)))
+        rvel(2*i-1) = dreal(vel(i))
+        rvel(2*i) = dimag(vel(i))
       enddo
 C$OMP END PARALLEL DO      
       return
@@ -153,18 +156,21 @@ c
       real *8, intent(out) :: errs(numit+1),rres
       complex *16, intent(out) :: soln(n)
 
-      complex *16 zid,ztmp
+      real *8 did,ztmp
       real *8 rb,wnrm2
       integer it,iind,it1,k,l
       real *8 rmyerr
-      complex *16 temp
-      complex *16, allocatable :: vmat(:,:),hmat(:,:)
-      complex *16, allocatable :: cs(:),sn(:)
-      complex *16, allocatable :: svec(:),yvec(:),wtmp(:)
+      real *8 temp
+      real *8, allocatable :: vmat(:,:),hmat(:,:)
+      real *8, allocatable :: cs(:),sn(:)
+      real *8, allocatable :: svec(:),yvec(:),wtmp(:),dsoln(:)
+      complex *16 ima
+      data ima/(0.0d0,1.0d0)/
 
-      allocate(vmat(n,numit+1),hmat(numit,numit))
+      allocate(vmat(2*n,numit+1),hmat(numit,numit))
       allocate(cs(numit),sn(numit))
-      allocate(wtmp(n),svec(numit+1),yvec(numit+1))
+      allocate(wtmp(2*n),svec(numit+1),yvec(numit+1))
+      allocate(dsoln(2*n))
 
       do i=1,numit+1
         errs(i) = 0
@@ -178,7 +184,7 @@ c       the identity scaling (z) is defined via zid below,
 c       and K represents the action of the principal value 
 c       part of the matvec
 c
-      zid = -0.5d0*dwgt
+      did = -0.5d0*dwgt
 
 
       niter=0
@@ -204,7 +210,8 @@ C$OMP END PARALLEL DO
 
 C$OMP PARALLEL DO DEFAULT(SHARED)
       do i=1,n
-        vmat(i,1) = rhs(i)/rb
+        vmat(2*i-1,1) = dreal(rhs(i))/rb
+        vmat(2*i,1) = dimag(rhs(i))/rb
       enddo
 C$OMP END PARALLEL DO      
 
@@ -228,45 +235,45 @@ c
         do k=1,it
           ztmp = 0
 C$OMP PARALLEL DO DEFAULT(SHARED) REDUCTION(+:ztmp)          
-          do j=1,n
-            ztmp = ztmp + wtmp(j)*conjg(vmat(j,k))
+          do j=1,2*n
+            ztmp = ztmp + wtmp(j)*vmat(j,k)
           enddo
 C$OMP END PARALLEL DO          
           hmat(k,it) = ztmp
 
 C$OMP PARALLEL DO DEFAULT(SHARED) 
-          do j=1,n
+          do j=1,2*n
             wtmp(j) = wtmp(j)-hmat(k,it)*vmat(j,k)
           enddo
 C$OMP END PARALLEL DO          
         enddo
           
-        hmat(it,it) = hmat(it,it)+zid
+        hmat(it,it) = hmat(it,it)+did
         wnrm2 = 0
 C$OMP PARALLEL DO DEFAULT(SHARED) REDUCTION(+:wnrm2)        
-        do j=1,n
+        do j=1,2*n
           wnrm2 = wnrm2 + abs(wtmp(j))**2
         enddo
 C$OMP END PARALLEL DO        
         wnrm2 = sqrt(wnrm2)
 
 C$OMP PARALLEL DO DEFAULT(SHARED) 
-        do j=1,n
+        do j=1,2*n
           vmat(j,it1) = wtmp(j)/wnrm2
         enddo
 C$OMP END PARALLEL DO        
 
         do k=1,it-1
-          temp = cs(k)*hmat(k,it)+conjg(sn(k))*hmat(k+1,it)
+          temp = cs(k)*hmat(k,it)+sn(k)*hmat(k+1,it)
           hmat(k+1,it) = -sn(k)*hmat(k,it)+cs(k)*hmat(k+1,it)
           hmat(k,it) = temp
         enddo
 
         ztmp = wnrm2
 
-        call zrotmat_gmres(hmat(it,it),ztmp,cs(it),sn(it))
+        call rotmat_gmres(hmat(it,it),ztmp,cs(it),sn(it))
           
-        hmat(it,it) = cs(it)*hmat(it,it)+conjg(sn(it))*wnrm2
+        hmat(it,it) = cs(it)*hmat(it,it)+sn(it)*wnrm2
         svec(it1) = -sn(it)*svec(it)
         svec(it) = cs(it)*svec(it)
         rmyerr = abs(svec(it1))/rb
@@ -296,10 +303,10 @@ c
 c          estimate x
 c
 C$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(i,j)
-          do j=1,n
-            soln(j) = 0
+          do j=1,2*n
+            dsoln(j) = 0
             do i=1,it
-              soln(j) = soln(j) + yvec(i)*vmat(j,i)
+              dsoln(j) = dsoln(j) + yvec(i)*vmat(j,i)
             enddo
           enddo
 C$OMP END PARALLEL DO          
@@ -307,7 +314,7 @@ C$OMP END PARALLEL DO
 
           rres = 0
 C$OMP PARALLEL DO DEFAULT(SHARED)          
-          do i=1,n
+          do i=1,2*n
             wtmp(i) = 0
           enddo
 C$OMP END PARALLEL DO          
@@ -318,18 +325,25 @@ c        evaluation routine
 c
 
       call lpcomp_stok_dl_interior(n,src,dsdt,rn,rkappa,dwgt,pwgt,
-     1   eps,soln,wtmp)
+     1   eps,dsoln,wtmp)
 
 C$OMP PARALLEL DO DEFAULT(SHARED) REDUCTION(+:rres)            
           do i=1,n
-            rres = rres + abs(zid*soln(i) + wtmp(i)-rhs(i))**2
+            rres = rres + abs(did*dsoln(2*i-1)+wtmp(2*i-1)
+     1         -dreal(rhs(i)))**2
+            rres = rres + abs(did*dsoln(2*i)+wtmp(2*i)
+     1         -dimag(rhs(i)))**2
           enddo
 C$OMP END PARALLEL DO          
           rres = sqrt(rres)/rb
           niter = it
+          do i=1,n
+            soln(i) = dsoln(2*i-1) + ima*dsoln(2*i)
+          enddo
           return
         endif
       enddo
+
 c
       return
       end
@@ -337,6 +351,60 @@ c
 c
 c
 c
+
+c
+c  This file contains the following user-callable routines
+c  
+c    - rotmat_gmres: 
+c        compute the sin and cos such that tan = b/a, with stability
+c        close to 0,1
+c    - zrotmat_gmres:
+c        complex version of rotmat_gmres
+c
+
+
+
+      subroutine rotmat_gmres(a,b,c,s)
+c
+c-----------------------
+c  Given a,b, compute sin(theta), cos(theta),
+c  such that tan(theta) = b/a, note, the routine
+c  implements a stabler version of computing
+c  b/sqrt(a^2+b^2) and a/sqrt(b^2+a^2)
+c
+c  Input arguments:
+c  
+c    - a: double precision
+c        cos scaling
+c    - b: double precision
+c        sin scaling
+c
+c  Output arguments:
+c    - c: double precision
+c        cos(theta)
+c    - s: double precision
+c        sin(theta)
+c        
+c
+c-----------------------
+c
+      implicit real *8 (a-h,o-z)
+      real *8 a,b,c,s
+
+      if(a.eq.0) then
+        c = 0
+        s = 1
+      else
+        temp = b/a
+        c = 1.0d0/sqrt(1.0d0+temp**2)
+        s = temp*c
+
+      endif
+
+      return
+      end
+
+          
 
 
 
