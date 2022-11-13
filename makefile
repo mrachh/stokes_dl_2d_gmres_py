f5ffa377959f76b81132a4b141a990cdbe50a75f
Manas Rachh <mrachh@flatironinstitute.org>
2022-11-13 16:19:45 -0500

    moving to make system

---
+++ makefile
@@ -0,0 +1,160 @@
+# Makefile for fmm3dbie
+# # This is the only makefile; there are no makefiles in subdirectories.
+# Users should not need to edit this makefile (doing so would make it
+# hard to stay up to date with repo version). Rather in order to
+# change OS/environment-specific compilers and flags, create 
+# the file make.inc, which overrides the defaults below (which are 
+# for ubunutu linux/gcc system). 
+
+# compiler, and linking from C, fortran
+CC = gcc
+CXX = g++
+FC = gfortran
+FFLAGS = -fPIC -O3 -march=native -funroll-loops -std=legacy 
+
+# extra flags for multithreaded: C/Fortran, MATLAB
+OMPFLAGS =-fopenmp
+OMPLIBS =-lgomp 
+
+FMM_INSTALL_DIR=$(PREFIX_FMM)
+ifeq ($(PREFIX_FMM),)
+	FMM_INSTALL_DIR=${HOME}/lib
+endif
+
+
+LIBS = -lm
+DYLIBS = -lm
+F2PYDYLIBS = -lm 
+
+LIBNAME=$(PREFIX_LIBNAME)
+ifeq ($(LIBNAME),)
+	LIBNAME=libstokes2ddl
+endif
+
+DYNAMICLIB = $(LIBNAME).so
+STATICLIB = $(LIBNAME).a
+LIMPLIB = $(DYNAMICLIB)
+
+LFMMLINKLIB = -lfmm2d
+LLINKLIB = $(subst lib, -l, $(LIBNAME))
+
+
+# For your OS, override the above by placing make variables in make.inc
+-include make.inc
+
+# update libs and dynamic libs to include appropriate versions of
+# fmm3d
+#
+# Note: the static library is used for DYLIBS, so that fmm3d 
+# does not get bundled in with the fmm3dbie dynamic library
+#
+LIBS += -L$(FMM_INSTALL_DIR) $(LFMMLINKLIB) 
+DYLIBS += -L$(FMM_INSTALL_DIR) $(LFMMLINKLIB)
+F2PYDYLIBS += -L$(FMM_INSTALL_DIR) $(LFMMLINKLIB)
+
+# multi-threaded libs & flags needed
+ifneq ($(OMP),OFF)
+  FFLAGS += $(OMPFLAGS)
+  LIBS += $(OMPLIBS)
+  DYLIBS += $(OMPLIBS)
+  F2PYDYLIBS += $(OMPLIBS)
+endif
+
+
+
+# objects to compile
+#
+ifeq ($(BLAS_64),ON)
+COMOBJS += $(COM)/lapack_wrap_64.o
+endif
+
+ifneq ($(BLAS_64),ON)
+COMOBJS += $(COM)/lapack_wrap.o
+endif
+
+
+OBJS = stokes_sherman_lauricella_solver.o  
+
+
+
+
+.PHONY: usage lib test python 
+
+default: usage
+
+usage:
+	@echo "-------------------------------------------------------------------------"
+	@echo "Makefile for stokes 2d sherman lauricella solver. Specify what to make:"
+	@echo "  make lib - compile the main library (in lib/ and lib-static/)"
+	@echo "  make test - compile and run validation tests (will take around 30 secs)"
+	@echo "  make python - compile and test python interfaces using python"
+	@echo "  make objclean - removal all object files, preserving lib & MEX"
+	@echo "  make clean - also remove lib, MEX, py, and demo executables"
+	@echo ""
+	@echo "For faster (multicore) making, append the flag -j"
+	@echo "  'make [task] OMP=ON' for multi-threaded"
+	@echo "-------------------------------------------------------------------------"
+
+
+
+#
+# implicit rules for objects (note -o ensures writes to correct dir)
+#
+%.o: %.f %.h
+	$(FC) -c $(FFLAGS) $< -o $@
+%.o: %.f90 
+	$(FC) -c $(FFLAGS) $< -o $@
+
+
+
+#
+# build the library...
+#
+lib: $(STATICLIB) $(DYNAMICLIB)
+ifneq ($(OMP),OFF)
+	@echo "$(STATICLIB) and $(DYNAMICLIB) built, multithread versions"
+else
+	@echo "$(STATICLIB) and $(DYNAMICLIB) built, single-threaded versions"
+endif
+
+$(STATICLIB): $(OBJS) 
+	ar rcs $(STATICLIB) $(OBJS)
+	mv $(STATICLIB) lib-static/
+
+$(DYNAMICLIB): $(OBJS) 
+	$(FC) -shared -fPIC $(FFLAGS) $(OBJS) -o $(DYNAMICLIB) $(DYLIBS) 
+	mv $(DYNAMICLIB) lib/
+	[ ! -f $(LIMPLIB) ] || mv $(LIMPLIB) lib/
+
+
+#
+# testing routines
+#
+test: $(STATICLIB) testf 
+	./int2
+
+testf: 
+	$(FC) $(FFLAGS) bhgmres_dr.f -o int2 lib-static/$(STATICLIB) $(LIBS) 
+
+
+#
+# build the python bindings/interface
+#
+python: $(STATICLIB)
+	cd python && export FMMBIE_LIBS='$(LIBS)' && pip install -e . 
+
+
+
+#
+# housekeeping routines
+#
+clean: objclean
+	rm -f lib-static/*.a lib/*.so
+	rm -f int2
+	rm -f test/tria_routs/int2-tria
+	rm -f python/*.so
+	rm -rf python/build
+	rm -rf python/*.egg-info
+
+objclean: 
+	rm -f $(OBJS) 
--- stokes_sherman_lauricella_solver.f
+++ stokes_sherman_lauricella_solver.f
@@ -450,3 +450,247 @@ c-----------------------
           
 
 
+c---------------------------------------------------------------------      
+      
+      subroutine uexact(itype,xpos,ypos,cx,cy,xvel,yvel,vort,
+     1                  pressure,ifw,w)
+c      This subroutine evaluates a particular stokes field
+c      corresponding to W = x log r where r = sqrt(x**2 + y**2)
+c      if itype = 1 and if itype =0, it returns the field
+c      phi = 1/zdis and psi = 3/zdis where zdis is the
+c      distance from the source located at (cx,cy)
+c      ARGUMENTS
+c      ------------------------------------------------------
+c      itype     IN: integer
+c                type of boundary data to be picked up
+c
+c      xpos      In: real *8
+c                x coordinate of target location where field
+c                is to be evaluated
+c
+c      ypos      In: real *8
+c                y coordinate of target location where field
+c                is to be evaluated
+c
+c      cx        In: real *8
+c                x coordinate of source location
+c
+c      cy        In: real *8
+c                y coordinate of source location
+c
+c      ifw       In: integer
+c                flag to compute the biharmonic potential
+c--------------------------------------------------------------
+c      OUTPUT
+c      xvel      Out: real *8
+c                x component of the velocity at the target
+c      yvel      Out: real *8
+c                y component of the velocity at the target
+c      w         Out: real *8
+c                biharomnic potential at the target
+c -------------------------------------------------------------
+
+      implicit none
+
+      integer itype,ifw
+      real *8 xpos,ypos,cx,cy,xvel,yvel,vort,pressure,w
+      complex *16 ztarg,zdis,zdis2
+      complex *16 phi,phip,psi,gw,chi,bipot
+
+      ztarg = dcmplx(xpos,ypos)
+      zdis = ztarg - dcmplx(cx,cy)
+
+
+c     Option 0: decaying solution at infinity
+      if(itype.eq.0) then
+         phi = 1.0d0/zdis
+         phip = -1.0d0/(zdis*zdis)
+         psi = 3.0d0/zdis
+         chi = 3.0d0*log(zdis)
+
+         gw = phi + ztarg*dconjg(phip)+dconjg(psi)
+         vort = 4*dreal(phip)
+         pressure = 4*dimag(phip)
+
+         xvel = dimag(gw)
+         yvel = -dreal(gw)
+         if(ifw.eq.1)  w = dreal(dconjg(ztarg)*phi + chi)
+      else
+         yvel = -log(cdabs(ztarg)) - xpos**2/cdabs(ztarg)**2
+         xvel = xpos*ypos/cdabs(ztarg)**2
+         vort = -2.0d0*xpos/cdabs(ztarg)**2
+         if(ifw.eq.1) w = xpos*log(cdabs(ztarg))
+      endif
+         
+
+      return
+      end
+c ----------------------------------------------------------------      
+
+      subroutine direval(zx,zvort,zvel,k0,k,nd,xs,ys,rnx,rny,
+     1                   rkappa,dsdt,h,wdens,swgt,dwgt,cwgt,pwgt)
+c     Given complex density wdens compute the velocity corresponding
+c     to the SL+DL representation for Stokes equation. 
+c  
+c     All the integrals in the representation below are computed
+c     using trapezoidal rule.
+c
+C     The stream function W  is represented
+C     by the complex variables formula
+C
+C           W = Re( CONJG(Z)*PHI + XI)
+C     with 
+c           W_x + i W_y = PHI + Z*CONJG(PHI') + CONJG(PSI)
+c     where u = W_y and v = -W_x are the x and the y components
+c     of velocity respectively
+c
+c     where PSI = XI'. The modified Sherman-Lauricella equation
+c     is based on the following representation.
+c
+c     \phi(z) = -swgt( \int [w(t) log (t-z) ds(t)])/ (8 \pi) 
+c               -dwgt( \int [w(t) / (t-z) dt])/ (4 \pi i)
+c               + cwgt (\int [w(t) ds(t) ]). (2 \pi) 
+c
+c     where z,t are complex variables, w(t) is a complex density,
+c
+c     for any variable x, let xb be dconjg(x)
+c     \psi(z) = -swgt(\int [wb log (t-z) ds(t) +
+c                    tb*w*ds(t)/(t-z)])/(8\pi)
+c               - dwgt(\int [(wb*dt + w*dtb)/(t-z) 
+c                      - tb*w(t)/(t-z)**2])/(4 \pi i)
+c
+c-----------------------------------------------------------------
+c     INPUT ARGUMENTS
+c     zx       In: complex *16
+c              zx = xt + i yt. Location of target in complex
+c              notation
+C
+C     k0,k      in: INTEGER
+C
+C               k0 = 0 for interior and 1 for exterior problems.
+C               k  = number of obstacles.
+c 
+C     nd(0:k)  in: integer
+C
+C               ND(J) = number of points in discretization of
+C                       Jth boundary component.
+C
+C**********************************************************************
+C     For future reference we define ntot = SUM_{J=K0}^K ND(J)
+C**********************************************************************
+C
+C     The x and y coordinates of the points defining the boundary
+C     components are given by
+C
+C     xs(ntot),ys(NTOT) IN: REAL *8 
+C
+C               xs(1,...,ND(K0)) xcoordinates of K0th boundary component.
+C               ys(1,...,ND(K0)) xcoordinates of K0th boundary component.
+C
+C               ...
+C
+C               xs(NTOT-ND(K)+1,...,NTOT) xcoords of Kth boundary.
+C               ys(NTOT-ND(K)+1,...,NTOT) xcoords of Kth boundary.
+C
+C**********************************************************************
+C     They are assumed to be equispaced with respect to
+C     some parameter theta (see dsdtheta below).
+C**********************************************************************
+C
+C     dsdt(NTOT) IN: REAL *8 
+C
+C               ds/d(theta) where s denotes arclength and theta is the 
+C               parametrization of the curve in which the points are 
+C               equispaced (using the same ordering as the X,Y array).
+c
+C     rnx(NTOT),rny(NTOT) IN: REAL *8 
+C
+C               x and y components of outward normal at corresponding
+C               point in xs,ys array.
+C
+C     rkappa(NTOT) IN: REAL *8 
+C
+C               curvature at corresponding point in xs,ys array.
+C
+C
+C     h(0:K)   IN: REAL *8 
+C
+C               weight of trapezoidal rule, that is h(J) is length of 
+C               Jth boundary measured in parameter theta, divided by 
+C               nd(J).
+C     wdens(1:ntot)   In: complex *16
+c               The density for SL + DL in the above representation
+c
+C     swgt      in: real *8
+c               Weight of the single layer in the representation for
+c               velocity
+c
+c     dwgt      in: real *8
+c               Weight of the double layer in the representation for
+c               velocity
+c
+c     cwgt      in: real *8
+c               Weight of scaled integral of density in the
+c               representation for velocity (Check documentation above)
+c      
+c     pwgt      in: real *8
+c               Weight of generalized 1's matrix to compensate
+c               for the nullspace in the representation for interior
+c               flows
+c-------------------------------------------------------------------
+c     OUTPUT arguments
+c     zvort     out: complex*16
+c               zvort = vort + i pressure at the target
+c 
+c     zvel      out: complex *16
+c               complex velocity  -v + i u at the target
+c-------------------------------------------------------------------
+
+      implicit none
+      integer k0,k,nd(0:k)
+      integer i,ii,istart,nbod
+
+      real *8 xs(*),ys(*),dsdt(*),rnx(*),rny(*),rkappa(*)
+      real *8 h(0:k),swgt,dwgt,cwgt,pwgt
+
+      real *8 pi
+
+      complex *16 zn,zx,zvel,zvort,zdis,eye,zsc,zu,wdens(*)
+
+      zvel = dcmplx(0.0d0,0.0d0)
+      zvort = dcmplx(0.0d0,0.0d0)
+      eye = dcmplx(0.0d0,1.0d0)
+      pi = 4.0d0*datan(1.0d0)
+      istart = 0
+
+      do nbod = k0,k
+         do i=1,nd(nbod)
+            ii = i+istart
+            zn = dcmplx(rnx(ii),rny(ii))
+            zdis = dcmplx(xs(ii),ys(ii)) - zx
+            zu = wdens(ii)
+            zsc = h(nbod)*dsdt(ii)/(2.0d0*pi*eye)
+
+
+            zvel = zvel - dwgt*0.5d0*zsc*eye*zn*zu/zdis
+            zvel = zvel + dwgt*0.5d0*zsc*eye*dconjg(zn*zu/zdis**2)*
+     1                     zdis
+            zvel = zvel + dwgt*zsc*dreal(eye*zn*dconjg(zu))/
+     1                     dconjg(zdis)
+
+         enddo
+         istart = istart + nd(nbod)
+      enddo
+      zvort = zvort*4
+
+cc      call prin2('const=*',wdens(istart+1),2)
+cc      call prinf('istart+1=*',istart+1,1)
+
+      if(k0.eq.1) then
+         zvel = zvel + wdens(istart+1)
+      endif
+
+      return
+      end
+
+c------------------------------------------------------------------
